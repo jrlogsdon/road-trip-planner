ddad29234887d0613aa700862677a9014d6d1643
Jacob Logsdon <jrlogsdon21@gmail.com>
2021-11-14 14:25:00 -0500

    cleaning up outputs for the user

--- app.py
+++ app.py
@@ -8,15 +8,23 @@ api_key = input("Enter your Google API Key containing access to Distance Matrix 
 
 locations = user.get_locations()
 
-distance_url = google_distance_matrix_service.build_distance_url(locations, locations, key=api_key)
+if len(locations) > 2:
 
-dest_matrix = google_distance_matrix_service.get_distance_matrix_from_api(distance_url)
+    distance_url = google_distance_matrix_service.build_distance_url(locations, locations, key=api_key)
 
-graph = parse_response_to_graph.create_graph(dest_matrix)
+    dest_matrix = google_distance_matrix_service.get_distance_matrix_from_api(distance_url)
 
-order_to_visit_nodes = explorer.explore(graph=graph, starting_location=dest_matrix['origin_addresses'][0])
+    graph = parse_response_to_graph.create_graph(dest_matrix)
 
-print("Most efficient way to visit nodes: \n", order_to_visit_nodes)
+    order_to_visit_nodes = explorer.explore(graph=graph, starting_location=dest_matrix['origin_addresses'][0])
+    print("Best route is detailed below \n\n")
+    print("Start at: ", order_to_visit_nodes[0])
+    for i in range(1, len(order_to_visit_nodes) - 1):
+        print("Then travel to ", order_to_visit_nodes[i])
+
+    print("Finally end at ", order_to_visit_nodes[len(order_to_visit_nodes) -1])
+else:
+    print("You did not specify enough locations for a road trip")
 
 distance_matrix_csv_writer.write_csv(dest_matrix, "anotherone.csv")
 
