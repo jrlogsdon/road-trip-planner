06cfe8f02e3f891839e2d64c25b9b607bc134565
Jacob Logsdon <jrlogsdon21@gmail.com>
2021-11-18 12:34:40 -0500

    changing wording for first input

--- user.py
+++ user.py
@@ -3,10 +3,17 @@
 def get_locations():
     locations = []
     location = 'not_empty'
+    is_first = True
     while is_valid(location):
-        location = str(input("Enter location you'd like to visit (enter no or newline to stop entering locations): "))
-        if is_valid(location):
-            locations.append(location)
+        if is_first:
+            location = str(input("Enter a starting location: "))
+            if is_valid(location):
+                locations.append(location)
+                is_first = False
+        else:
+            location = str(input("\nEnter location you'd like to visit (enter no or newline to stop entering destinations): "))
+            if is_valid(location):
+                locations.append(location)
     return locations
 
 
